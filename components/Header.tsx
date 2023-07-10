4071b6fe8fc0164b3e4d2cb1c2e4b6e8eb47339a
Jordy <jbonn2002@gmail.com>
2023-07-10 12:32:04 -0700

    first commit of framer ai project

---
+++ components/Header.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Button } from "./ui/button";
+
+const Header = () => {
+  return (
+    <section className="flex flex-col bg-[#F0EAD2] items-center justify-between p-24">
+      <div>Image Holder</div>
+      <div className="relative flex text-center place-items-center md:text-6xl text-4xl">
+        Revolutionize Your Business
+      </div>
+      <div className="mt-10">
+        <Button className="mx-2 text-base bg-[#A98467]">Learn More</Button>
+        <Button className="mx-2 text-base bg-[#ADC178]">Get Started</Button>
+      </div>
+    </section>
+  );
+};
+
+export default Header;
